c88505daeab49a678882cf50b18fba5eb3699013
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2021-02-26 14:06:23 +0000

    ts-mocha+chai stub

---
+++ tests/base.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+// Basic stub test, just ensures ts-mocha+chai runs
+describe("Test testing", () => {
+    it("Runs successfully", () => {
+        expect(true).to.be.true
+    })
+});
